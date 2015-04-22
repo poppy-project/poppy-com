d966b0f2bece791bd4805d732c0f6625b277d692
Nicolas <nicolas.rabault@inria.fr>
2015-04-22 16:49:56 +0200

    Add frontend documentation

--- poppy-com/poppyNetwork.h
+++ poppy-com/poppyNetwork.h
@@ -1,31 +1,86 @@
+/**
+ * \file poppyNetwork.h
+ * \brief Poppy communication main include file.
+ * \author Nicolas Rabault
+ * \version 0.1
+ * \date 22 Avril 2015
+ *
+ * Include this file to use the poppy communication protocole.
+ *
+ */
+
 #ifndef poppy_com_H_
 #define poppy_com_H_
 
+/**
+ * \enum msg_dir_t
+ * \brief Message direction enum.
+ *
+ * This structure is used to get the message direction but it seems to be useles
+ * because we have defferent interrupt for each msg_dir case.
+ */
 typedef enum {
-    TX,
-    RX,
-    RXGC
+    TX,   /*!< Slave transmiter mode. */
+    RX,   /*!< Slave receiver mode. */
+    RXGC  /*!< Slave receiver général call mode. */
 }msg_dir_t;
 
+
+/**
+ * \struct msg_t
+ * \brief Message structure.
+ *
+ * This structure is used to receive or send messages between modules in slave
+ * and master mode.
+ * please refer to ?? documentation
+ */
 typedef struct {
-    // Message register
-    unsigned char reg;
-    // Message size
-    unsigned char size;
-    // Data
-    unsigned char data[512];
+    unsigned char reg;        /*!< Message register. */
+    unsigned char size;       /*!< Message size. */
+    unsigned char data[512];  /*!< Data (512 bytes max). */
 }msg_t;
 
 typedef void (*RX_CB) (msg_dir_t dir, msg_t *msg);
 typedef void (*TX_CB) (msg_t *msg);
 
+/**
+ * \fn void poppyNetwork_init(TX_CB tx_cb, RX_CB rx_cb, RX_CB rxgc_cb)
+ * \brief Initialisation of the Poppy communication lib.
+ *
+ * \param tx_cb function pointer into the tx callback.
+ * \param rx_cb function pointer into the rx callback.
+ * \param rxgc_cb function pointer into the rx general call callback.
+ *
+ * TODO :
+ * - Return success or not.
+ * - Check NULL pointers.
+ */
 void poppyNetwork_init(TX_CB tx_cb,
                        RX_CB rx_cb,
                        RX_CB rxgc_cb);
+
+/**
+ * \fn unsigned char poppyNetwork_read(unsigned char addr, msg_t *msg)
+ * \brief Master mode read function.
+ *
+ * \param addr Address of the slave.
+ * \param msg Message to send to the slave, he come back with the reply of the slave.
+ *
+ * TODO :
+ * - Write before read management
+ */
 unsigned char poppyNetwork_read(unsigned char addr, msg_t *msg);
+
+/**
+ * \fn unsigned char poppyNetwork_write(unsigned char addr, msg_t *msg)
+ * \brief Master mode write function.
+ *
+ * \param addr Address of the slave.
+ * \param msg Message to send to the slave.
+ */
 unsigned char poppyNetwork_write(unsigned char addr, msg_t *msg);
 
-/*
+/**
  **TODO**
  *
  *Les trucs relatif au boot doivent être viré, cette lib doit êttre utilisé tant du coté bootloader qu'utilisateur...
--- template.c
+++ template.c
@@ -1,30 +1,67 @@
-/*
- * template.c
+/**
+ * \file template.c
+ * \brief Poppy module application side template.
+ * \author Nicolas Rabault
+ * \version 0.1
+ * \date 22 Avril 2015
  *
- * Created: 11/02/2015 12:40:48
- *  Author: Nicolas Rabault
+ * Please feel free to copy this template and use it at base for your new Poppy
+ * module application.
  */
 
+
+
+/**
+ * This is the minimal include you will need to use poppy-com in a module
+ * application
+ */
 #include "poppy-com/poppyNetwork.h"
 
+/**
+ * \fn void rx_cb(msg_dir_t dir, msg_t *msg)
+ * \brief Callback function for Slave mode messages reception.
+ *
+ * \param dir Message direction. (That will be remove!)
+ * \param msg Received message.
+ */
 void rx_cb(msg_dir_t dir, msg_t *msg) {
     /*
      * Add your RX code here.
      */
 }
 
+/**
+ * \fn void rxgc_cb(msg_dir_t dir, msg_t *msg)
+ * \brief Callback function for Slave mode messages reception with general call.
+ *
+ * \param dir Message direction. (That will be remove!)
+ * \param msg Received message.
+ */
 void rxgc_cb(msg_dir_t dir, msg_t *msg) {
     /*
      * Add your RX general call code here.
      */
 }
 
+/**
+ * \fn void tx_cb(msg_t *msg)
+ * \brief Callback function for Slave mode messages transmission.
+ *
+ * \param msg Transmitted message. You have to put something in this msg_t.
+ */
 void tx_cb(msg_t *msg) {
     /*
      * Add your TX code here.
      */
 }
 
+
+/**
+ * \fn int main(void)
+ * \brief Your main module application process.
+ *
+ * \return integer
+ */
 int main(void) {
     poppyNetwork_init(tx_cb, rx_cb, rxgc_cb);
 
