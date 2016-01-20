697740cddc6501a2c66208ce94c0bf809ad886a6
astralien3000 <astralien3000@yahoo.fr>
2016-01-20 17:54:40 +0100

    Added Cmake toolchain

---
+++ toolchain.cmake
@@ -0,0 +1,7 @@
+cmake_minimum_required(VERSION 2.6)
+cmake_policy(SET CMP0011 NEW)
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_C_COMPILER     avr-gcc)
+set(CMAKE_CXX_COMPILER   avr-g++)
+set(CMAKE_ASM_COMPILER   avr-gcc)
